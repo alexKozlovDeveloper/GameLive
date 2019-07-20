f27fcecd5a74f838eeb23b09cb2c2d24df5af60b
MINSK\Aliaksei_Kazlou <alexkozlov11g11@gmail.com>
2019-07-21 02:20:31 +0300

    Start Arena Phase

---
+++ GameLive.Web/GameLive.Web/Controllers/ArenaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameLive.Web.Controllers
+{
+    public class ArenaController : Controller
+    {
+        // GET: Arena
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
