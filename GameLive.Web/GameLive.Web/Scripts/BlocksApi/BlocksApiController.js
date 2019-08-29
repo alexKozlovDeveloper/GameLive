0f3afbd5b8fd4db80f0eac1549d54935db770590
MINSK\Aliaksei_Kazlou <alexkozlov11g11@gmail.com>
2019-08-30 00:51:58 +0300

    Update blocksApi

--- GameLive.Web/GameLive.Web/Scripts/BlocksApi/BlocksApiController.js
+++ GameLive.Web/GameLive.Web/Scripts/BlocksApi/BlocksApiController.js
@@ -3,22 +3,14 @@
     $canvas: null,
     canvasWidth: null,
     canvasHeight: null,
-    getStatisticsDiv: function(userName, killCount, deadCount) {
-    },
     addNewBlock: function (block) {
-
-
-        //top: 25px;
-        //left: 25px;
-        //width: 100px;
-        //height: 100px;
-        //background - image: url(../../ Content / Arena / Images / Flamingo - Star - Ship.png);
-
-        debugger;
+        this.addNewBlockRec(block, this.$canvas, 2);
+    },
+    addNewBlockRec: function (block, $parent, zIndex) {
 
         var blockDiv = "<div id=\"" + block.id + "\"></div>";
 
-        this.me.$canvas.append(blockDiv);
+        $parent.append(blockDiv);
 
         var $blockDiv = $("#" + block.id);
         $blockDiv.addClass("blocks-block");
@@ -29,42 +21,25 @@
         $blockDiv.css("top", block.y + "px");
         $blockDiv.css("left", block.x + "px");
 
+        $blockDiv.css("z-index", zIndex);
+
         $blockDiv.css("background-image", "url(" + block.image + ")");
         $blockDiv.css("transform", "rotate(" + block.angle + "deg)");
-        //var style = "style = 'position: relative; bottom: 10px; left: 10px; transform: rotate(15deg)'";
 
-        //var divStr = "<div class='star-ship'>";
-        //divStr += "<img src='/Content/Arena/Images/Piranha Star Ship.png' " + style + " />";
-        //divStr += "</div>";
-
-        //var blockDiv = "<div id=\"" + blockId + "\"><img id=\"" + blockId + "_img\" src='/Content/Arena/Images/Piranha Star Ship.png'/></div>";
-
-        //this.me.$canvas.append(blockDiv);
-
-        //var $blockDiv = $("#" + blockId);
-        //$blockDiv.addClass("block-div");
-
-        //var $blockDivImg = $("#" + blockId + "_img");
-
-        //$blockDivImg.css("position", "relative");
-
-        //$blockDivImg.css("width", "50px");
-        //$blockDivImg.css("height", "50px");
-
-        //$blockDivImg.css("bottom", "25px");
-        //$blockDivImg.css("left", "25px");
-
-        //$blockDivImg.css("background-color", "white");
-
-        //$blockDivImg.css("transform", "rotate(15deg)");
+        if (block.children !== null && block.children !== undefined) {
+            for (var i = 0; i < block.children.length; i++) {
+                this.addNewBlockRec(block.children[i], $blockDiv, zIndex + 1);
+            }
+        }
     },
     init: function (canvasSelector, width, height) {
-
-        debugger;
 
         window.BlocksAPiController.me = window.BlocksAPiController;
 
         this.me.$canvas = $(canvasSelector);
+
+        this.me.canvasWidth = width;
+        this.me.canvasHeight = height;
 
         this.me.$canvas.css("background-color", "black");
         this.me.$canvas.css("width", width + "px");
